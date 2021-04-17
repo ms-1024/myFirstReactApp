8898fdd494575d7d5162a0d8b759c357f349e00a
Marcin Stalmach <mstalmach@op.pl>
2021-04-17 16:31:12 +0200

    Add chapter 12.1

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -43,7 +43,7 @@ class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imageUrl={this.props.image} />
+        <Hero titleText={this.props.title} src={this.props.image} />
         <div className={styles.description}>
           {ReactHtmlParser(this.props.description)}
         </div>
